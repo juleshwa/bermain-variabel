f1af1d4150238dfc30935fbbee52622044c6c6bc
juleshwa <juleshwa@gmail.com>
2021-07-21 19:13:15 +0700

    Tugas JavaScript 21 Juli 2021

---
+++ tugas.js
@@ -0,0 +1,67 @@
+/* Tugas I
+1. Mari membentuk sebuah kalimat dengan menggabung-gabungkan kata yang disimpan dalam variabel
+2. Hasil yang muncul (output) dalam console: JavaScript itu asyik dan aku senang banget!
+*/
+var kataPertama = "JavaScript"
+// Tulis kode kamu di bawah ini
+
+// Contoh penggabungan kata-kata
+console.log('Hello' + ' ' + 'World!')
+
+/* Tugas II
+1. Mengakses setiap kata dalam kalimat berdasarkan index dan menyimpannya dalam variabel
+2. Hasil yang muncul (output dalam console):
+        First Word: Asik
+        Second Word: JavaScript
+        Third Word: sungguh
+        Fourth Word: keren
+        Fifth Word: banget!
+*/
+
+var kalimat = 'Asik JavaScript sungguh keren banget!';
+var contohFirstWord = kalimat[1] + kalimat[2] + kalimat[3] + kalimat[4]; // Hasilnya adalah Asik
+
+// tuliskan kode kalian di sini
+
+console.log('First Word: ' + contohFirstWord);
+// tuliskan kode kalian di sini
+
+/* Tugas III
+1. Memecah kata dalam kalimat menggunakan  Substring
+2. Hasil dalam console / output: 
+        First Word: wow
+        Second Word: JavaScript
+        Third Word: is
+        Fourth Word: so
+        Fifth Word: cool 
+*/
+
+var kalimatDua = "Asik JavaScript sungguh keren banget!";
+// contoh pemenggalan kalimat
+var exampleFirstWord3 = kalimatDua.substring(0, 4); // Hasilnya Asik
+// Tulis kode anda di sini
+
+console.log('First Word: ' + exampleFirstWord3);
+// Tulis kode ada di sini
+
+/* Tugas IV
+1. Memecahkan kalimat perkata dan menghitung banyaknya huruf dengan properti 'length'
+2. Hasil di console / output:
+    First Word: wow, with length: 3
+    Second Word: JavaScript, with length: 10
+    Third Word: is, with length: 2
+    Fourth Word: so, with length: 2
+    Fifth Word: cool, with length: 4
+*/
+
+var kalimatTiga = 'wow JavaScript is so cool';
+var exampleFirstWord4 = kalimatTiga.substring(0, 4); // Hasilnya asik
+// tulis kode anda di sini
+
+var firstWordLength = exampleFirstWord4.length; // Jumlah karakter dalam kata Asik
+// tulis kode anda di sini
+
+console.log(
+    'First Word: ' + exampleFirstWord + ', with length: ' + firstWordLength
+);
+// tulis kode anda di sini

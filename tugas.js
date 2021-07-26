152fbebf8f17f4f34b18a5cd933a4673152771d2
rmandalay <rivaldomanss7@gmail.com>
2021-07-26 08:18:38 +0700

    Tugas 1 Rivaldo

--- tugas.js
+++ tugas.js
@@ -17,6 +17,10 @@ console.log(kataPertama + ' ' + kataKedua + ' ' + kataKetiga + ' ' + kataKeempat
 // Contoh penggabungan kata-kata
 console.log('Hello' + ' ' + 'World!')
 
+console.log('')
+console.log('----------')
+console.log('')
+
 /* Tugas II
 1. Mengakses setiap kata dalam kalimat berdasarkan index dan menyimpannya dalam variabel
 2. Hasil yang muncul (output dalam console):
@@ -28,12 +32,25 @@ console.log('Hello' + ' ' + 'World!')
 */
 
 var kalimat = 'Asik JavaScript sungguh keren banget!';
-var contohFirstWord = kalimat[1] + kalimat[2] + kalimat[3] + kalimat[4]; // Hasilnya adalah Asik
-
+var contohFirstWord = kalimat[0] + kalimat[1] + kalimat[2] + kalimat[3]; // Hasilnya adalah Asik
+console.log('TUGAS 2')
 // tuliskan kode kalian di sini
+let contohSecondWord = kalimat[4] + kalimat[5] + kalimat[6] + kalimat[7] + kalimat[8] + kalimat[9] + kalimat[10] + kalimat[11] + kalimat[12] + kalimat[13] + kalimat[14];
+let contohThirdWord = kalimat[15] + kalimat[16] + kalimat[17] + kalimat[18] + kalimat[19] + kalimat[20] + kalimat[21] + kalimat[22];
+let contohFourthWord = kalimat[23] + kalimat[24] + kalimat[25] + kalimat[26] + kalimat[27] + kalimat[28];
+let contohFifthWord = kalimat[29] + kalimat[30] + kalimat[31] + kalimat[32] + kalimat[33] + kalimat[34] + kalimat[35] + kalimat[36]; 
 
 console.log('First Word: ' + contohFirstWord);
 // tuliskan kode kalian di sini
+console.log(`First word: ${contohFirstWord}.`);
+console.log(`Second word: ${contohSecondWord}.`);
+console.log(`Third word: ${contohThirdWord}.`);
+console.log(`Fourth word: ${contohFourthWord}.`);
+console.log(`Fifth word: ${contohFifthWord}.`);
+
+console.log('')
+console.log('---------')
+console.log('')
 
 /* Tugas III
 1. Memecah kata dalam kalimat menggunakan  Substring
@@ -44,12 +61,11 @@ console.log('First Word: ' + contohFirstWord);
         Fourth Word: keren
         Fifth Word: banget!
 */
-
+console.log('TUGAS 3')
 var kalimatDua = "Asik JavaScript sungguh keren banget!";
 // contoh pemenggalan kalimat
 var exampleFirstWord3 = kalimatDua.substring(0, 4); // Hasilnya Asik
 // Tulis kode anda di sini
-
 let kataA = kalimatDua.substring(0, 4);
 let kataB = kalimatDua.substring(5, 15);
 let kataC = kalimatDua.substring(16, 23);
@@ -58,12 +74,15 @@ let kataE = kalimatDua.substring(30, 37);
 
 console.log('First Word: ' + exampleFirstWord3);
 // Tulis kode ada di sini
-
 console.log(`First word: ${kataA}.`);
 console.log(`Second word: ${kataB}.`);
 console.log(`Third word: ${kataC}.`);
 console.log(`Fourth word: ${kataD}.`);
 console.log(`Fifth word: ${kataE}.`);
+
+console.log('')
+console.log('----------')
+console.log('')
 
 /* Tugas IV
 1. Memecahkan kalimat perkata dan menghitung banyaknya huruf dengan properti 'length'
@@ -74,15 +93,34 @@ console.log(`Fifth word: ${kataE}.`);
     Fourth Word: keren, with length: 5
     Fifth Word: banget, with length: 6
 */
+console.log('TUGAS 4')
 
 var kalimatTiga = 'wow JavaScript is so cool';
 var exampleFirstWord4 = kalimatTiga.substring(0, 4); // Hasilnya asik
 // tulis kode anda di sini
+// let exampleSecondWord4 = kalimatTiga.substring(4, 15);
+// let exampleThirdWord4 = kalimatTiga.substring(15, 17);
+// let exampleFourthWord4 = kalimatTiga.substring(18, 20);
+// let exampleFifthWord4  = kalimatTiga.substring(21, 25);
 
-var firstWordLength = exampleFirstWord4.length; // Jumlah karakter dalam kata Asik
+var words = kalimatTiga.split(" ");
+for (var i = 0; i < words.length - 1; i++) {
+    console.log((i+1)+' Word ' + words[i] + ', with length ' + words[i].length);
+}
+
+ // Jumlah karakter dalam kata Asik
+//  let secondWordLength = exampleSecondWord4.length;
+//  let thirdWordLength = exampleThirdWord4.length;
+//  let fourthWordLength = exampleFourthWord4.length;
+//  let fifthWordLength = exampleFifthWord4.length;
+//  var firstWordLength = exampleFirstWord4.length;
+
 // tulis kode anda di sini
 
-console.log(
-    'First Word: ' + exampleFirstWord4 + ', with length: ' + firstWordLength
-);
+// console.log('First Word: ' + exampleFirstWord4 + ', with length: ' + firstWordLength);
+
 // tulis kode anda di sini
+// console.log('Second Word: ' + exampleSecondWord4 + ', with length: ' + secondWordLength);
+// console.log('Third Word: ' + exampleThirdWord4 + ', with length: ' + thirdWordLength);
+// console.log('Fourth Word: ' + exampleFourthWord4 + ', with length: ' + fourthWordLength);
+// console.log('Fifth Word: ' + exampleFifthWord4 + ', with length: ' + fifthWordLength);
